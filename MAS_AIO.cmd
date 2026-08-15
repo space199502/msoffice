4a7084be7b8db846911eab2a85588f11fbe1e542
space199502 <awtssako@gmail.com>
2026-08-15 16:34:19 +0800

    Update MAS_AIO.cmd

--- MAS_AIO.cmd
+++ MAS_AIO.cmd
@@ -378,7 +378,7 @@ echo ________________________________________________
 echo:
 if not %_unattended%==1 (
 echo [1] Contact BOSS JP ULIT for code and how to access the Activator.
-echo:
+echo [0] Exit and Cry...
 call :dk_color %_Green% "Choose a menu option using your keyboard [1,0] :"
 choice /C:1J0 /N
 if !errorlevel!==3 (start exit /b)
